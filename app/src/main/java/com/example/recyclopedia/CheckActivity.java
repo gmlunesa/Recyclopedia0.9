07574aa50e4427e3b741cb078030c717b2d318b5
Goldy Mariz <gmlunesa@gmail.com>
2017-02-21 00:53:56 +0800

    updated CheckActivity

--- app/src/main/java/com/example/recyclopedia/CheckActivity.java
+++ app/src/main/java/com/example/recyclopedia/CheckActivity.java
@@ -6,8 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CheckActivity extends AppCompatActivity {
+
+    // initialize the UI objects
+    TextView answer_textview;
+    TextView details_textview;
+    ImageView img_imageview;
+
+    Button continue_button;
+
+    int score;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +30,15 @@ public class CheckActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Game");
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // assign the different views
+        answer_textview = (TextView) findViewById(R.id.result);
+        details_textview = (TextView) findViewById(R.id.result_details);
+        img_imageview = (ImageView) findViewById(R.id.result_image);
+        continue_button = (Button) findViewById(R.id.result_button);
+
+
     }
 
 }
