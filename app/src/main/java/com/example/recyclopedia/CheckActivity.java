b9023e47661c946bc4ec7c6e69ed428b09de53be
Goldy Mariz <gmlunesa@gmail.com>
2017-02-21 02:04:22 +0800

    changed CheckActivity and GameOverActivity

--- app/src/main/java/com/example/recyclopedia/CheckActivity.java
+++ app/src/main/java/com/example/recyclopedia/CheckActivity.java
@@ -1,5 +1,7 @@
 package com.example.recyclopedia;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -37,6 +39,55 @@ public class CheckActivity extends AppCompatActivity {
         details_textview = (TextView) findViewById(R.id.result_details);
         img_imageview = (ImageView) findViewById(R.id.result_image);
         continue_button = (Button) findViewById(R.id.result_button);
+
+        //get the information from the previous activity
+        Intent in = getIntent();
+        score = (in.getIntExtra("score", 0));       // get the score
+        counter = in.getIntExtra("counter", 0);     // get the counter, which keeps track on how many times the player has played
+
+        // compare the user answer against the correct answer (defined by and retrieved from the Game object from our database)
+        if (in.getIntExtra("user_answer", 0) == in.getIntExtra("real_answer", 0)) {
+            answer_textview.setText("CORRECT"); // set the text
+            ++score;    // increment the score, since the user is correct
+        } else {
+            answer_textview.setText(" OOPS! ");
+        }
+
+        // display the details of the particular product
+        details_textview.setText(in.getStringExtra("details"));
+
+        //print the image, using the file name which is stored in the database
+        Resources res = getResources();
+        String img_name = in.getStringExtra("img");
+        int resID = res.getIdentifier(img_name , "drawable", getPackageName());
+        img_imageview.setImageResource(resID);
+
+        // if counter is 5, that means the game is done.
+        if (counter == 5) {
+            continue_button.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(getApplicationContext(), GameOverActivity.class); // start the Game Over activity
+
+                    in.putExtra("score", score);       // pass the score, to be displayed by the Game Over activity
+                    in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(in);
+                }
+            });
+        } else {
+            continue_button.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(getApplicationContext(), GameActivity.class);    // if the user hasn't played 5 times yet, start another Game Activity class
+                    in.putExtra("counter", counter); // pass the counter
+                    in.putExtra("score", score);    // pass the score
+                    in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(in);
+                }
+            });
+        }
 
 
     }
